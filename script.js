856086c155bb407439a735009e0134535d51ead5
George Kolesnikov <gkolesnikov88@gmail.com>
2021-02-27 06:28:04 +0300

    Added save movie data to local storage

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.row .seats:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 
 const count = document.getElementById('count');
 const total = document.getElementById('total');
@@ -7,10 +7,19 @@ const movieSelector = document.getElementById('movie');
 
 let ticketPrice = +movieSelector.value;
 
+// Set selected movie and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 // Update total and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat))
+    localStorage.setItem('selectedSeats', seatsIndex);
+    
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
@@ -19,6 +28,7 @@ function updateSelectedCount() {
 // Movie selet event
 movieSelector.addEventListener('click', e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
 
